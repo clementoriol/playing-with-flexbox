33b51e3172ffc0431d48e1eeb530f27f3e25e23b
Clément Oriol <clement@clementoriol.com>
2015-03-15 16:03:32 +0100

    Add Jade task

--- gulpfile.js
+++ gulpfile.js
@@ -15,17 +15,29 @@
 
 /* -- Gulp Requires ---------------------------------  */
 
-var gulp          = require('gulp');
+var gulp          = require('gulp'),
+    jade          = require('gulp-jade');
 
 /* -- Constants & Paths -----------------------------  */
 
 // Paths
 var paths = {
+  views_src: 'src/views/**/*.jade',
+  views_dest: 'dist/'
 }
-
 /* =====================================================
     Single Tasks
 =====================================================  */
+
+/* -- Jade Task -------------------------------------  */
+gulp.task('build-jade', function(){
+  return gulp
+    .src(paths.views_src)
+    .pipe(jade({
+      pretty:true
+    }))
+    .pipe(gulp.dest(paths.views_dest));
+});
 
 /* =====================================================
     Workflow Tasks
